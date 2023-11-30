2ce780eae3a4209f2b338e6b72007466da152c93
mukarrom21 <mukarrom21@gmail.com>
2023-11-30 18:44:13 +0600

    added instruction how to run this project in local mechine

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# assignment is in processing. it is not completed
+# Assignment 2: Mongoose Express CRUD Mastery
+
+**Objective:** It is a Node.js Express application with TypeScript as the programming language, integrated MongoDB with Mongoose for user data and order management. Integrated Zod to validate input data.
+
+- Check live link: <https://assignment2.mukarrom.com/>
+
+## How to run this application
+
+Before running the application, make sure you have the following installed on your machine:
+
+- Node.js
+- MongoDB
+
+### Getting Started
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/mukarrom21/level-2-assignment-2.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd user-management-app
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   yarn
+   ```
+
+4. Set up mongoDB:
+
+   - Make sure MongoDB is running on your local machine.
+   - create .env file in this project root and set variables like:
+
+     ```env
+     PORT=5000
+     DB_URI=mongodb://localhost:27017/your-database-name
+     SALT_ROUNDS=16
+     ```
+
+5. Run this application:
+
+   - run in development
+
+     ```env
+     yarn start:dev
+     ```
+
+   - run in production
+
+     ```env
+     yarn build
+     yarn start
+     ```
+
+The application should now be running at <http://localhost:5000>.
+
+## API Endpoints
+
+- GET /api/users: Get all users.
+- POST /api/users: Create a new user.
+- GET /api/users/:userId: Get a single user by userId.
+- PUT /api/users/:userId: Update a user by userId.
+- DELETE /api/users/:userId: Delete a user by userId.
+- PUT /api/users/:userId/orders: Add a new order to a user.
+- GET /api/users/:userId/orders: Get all orders of a user.
+- GET /api/users/:userId/orders/total-price: Get the total price of all orders for a user.
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,23 +1,37 @@
 import { IUser } from './user.interface'
 import { StaticUser, UserModel } from './user.model'
 
+// Service to create a new user
 const createUserService = async (userData: IUser) => {
+  // Create a new user document using the UserModel
   const user = new UserModel(userData)
+
+  // Save the new user document to the database
   const result = await user.save()
+
+  // Return the result of the save operation
   return result
 }
 
+// Service to retrieve all users
 const getAllUserService = async () => {
+  // Find all user documents in the UserModel
   const result = await UserModel.find()
+
   return result
 }
 
+// Service to retrieve a single user by userId
 const getSingleUserByUserIdService = async (id: string) => {
+  // Find a user document by userId in the StaticUser model, excluding the password field
   const result = await StaticUser.findOne({ userId: id }, { password: 0 })
+
   return result
 }
 
+// Service to update a user by userId
 const updateUserService = async (userId: string, updateData: IUser) => {
+  // Find and update a user document by userId in the StaticUser model
   const result = await StaticUser.findOneAndUpdate(
     { userId: userId },
     updateData,
@@ -26,10 +40,11 @@ const updateUserService = async (userId: string, updateData: IUser) => {
   return result
 }
 
+// Service to delete a user by userId
 const deleteUserService = async (userId: string) => {
-  console.log(userId)
+  // Find and delete a user document by userId in the StaticUser model
   const result = await StaticUser.findOneAndDelete({ userId: userId })
-  console.log(result)
+
   return result
 }
 
@@ -42,8 +57,11 @@ const addNewProductService = async (userId: string, updateData: IUser) => {
   return result
 }
 
+// Service to retrieve all products (orders) of a user
 const getAllProductsService = async (userId: string) => {
+  // Find a user document by userId in the StaticUser model, including only the 'orders' field
   const result = await StaticUser.findOne({ userId: userId }, { orders: 1 })
+
   return result
 }
 
